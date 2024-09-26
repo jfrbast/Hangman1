f20d0c8bb1ce7e1f0a746392dfcf81b75dc966f3
jfrbast <bastien.jouffre@ynov.com>
2024-09-26 09:55:49 +0200

    problèmes git éradiqués
    j'ai retrouvé mon code au bout de 2 heures de test de push et de recreation de git ...

--- main.go
+++ main.go
@@ -20,87 +20,132 @@ func ToLower(s string) string {
 	}
 	return result
 }
+
 func IsAlpha(s string) bool {
 	for i := 0; i < len(s); i++ {
-		if !(s[i] >= 97 && s[i] <= 122) {
+		if !(s[i] >= 'a' && s[i] <= 'z') {
 			return false
 		}
 	}
 	return true
 }
 
-//
-
-//
-
+// Variables globales
 var tableauMot []string
-var motatrouver []string
-var MotJeu = tableauMot[0]
-var Tablettre []string
-var Compteur = 0
+var motATrouver []string
+var motJeu string
+var lettresEssayees []string
+var essaisRestants = 10
 
-//
+func TryLetter() {
+	var input string
+	fmt.Println("Proposez une lettre : ")
+	fmt.Scanln(&input)
+	input = ToLower(input)
 
-//
+	if !IsAlpha(input) || len(input) != 1 {
+		fmt.Println("Ceci n'est pas une lettre valide.")
+		return
+	}
+
+	for _, l := range lettresEssayees {
+		if l == input {
+			fmt.Println("Vous avez déjà proposé cette lettre.")
+			return
+		}
+	}
+	lettresEssayees = append(lettresEssayees, input)
+
+	found := false
+	for i, char := range motJeu {
+		if input == string(char) {
+			motATrouver[i] = input
+			found = true
+		}
+	}
+
+	if found {
+		fmt.Println("Lettre trouvée !")
+	} else {
+		essaisRestants--
+		fmt.Println("Lettre incorrecte. Essais restants :", essaisRestants)
+	}
+
+	fmt.Println("Mot à deviner : ", strings.Join(motATrouver, " "))
+	fmt.Println("Lettres déjà proposées : ", strings.Join(lettresEssayees, ", "))
+}
+
+func TryMot() {
+	var input string
+	fmt.Println("Proposez le mot entier : ")
+	fmt.Scanln(&input)
+	input = ToLower(input)
+
+	if input == motJeu {
+		fmt.Println("Félicitations ! Vous avez deviné le mot :", motJeu)
+		os.Exit(0) // Fin du jeu
+	} else {
+		essaisRestants -= 2
+		fmt.Println("Mauvaise proposition. Essais restants :", essaisRestants)
+	}
+}
 
 func ChoixMot() {
-
 	data, err := os.ReadFile("Mots.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
-	mot := string(data)
-	tableauMot = strings.SplitAfter(mot, "\n")
 
-	var Random int
-	Random = rand.Intn(len(tableauMot))
-	MotJeu = tableauMot[Random]
-	fmt.Println(MotJeu)
+	mots := strings.Split(strings.TrimSpace(string(data)), "\n")
 
-	Lrand1 := rand.Intn(len(MotJeu))
-	Lrand2 := rand.Intn(len(MotJeu))
-	if Lrand2 == Lrand1 {
-		if Lrand2 == len(MotJeu) {
-			Lrand2 -= 1
-		} else {
-			Lrand2 += 1
-		}
+	for i := range mots {
+		mots[i] = strings.TrimSpace(mots[i])
 	}
-	for i, char := range MotJeu {
+	tableauMot = mots
+
+	rand.Seed(time.Now().UnixNano())
+	motJeu = tableauMot[rand.Intn(len(tableauMot))]
+	motATrouver = make([]string, len(motJeu))
+
+	Lrand1 := rand.Intn(len(motJeu))
+	Lrand2 := rand.Intn(len(motJeu))
+	for Lrand2 == Lrand1 {
+		Lrand2 = rand.Intn(len(motJeu))
+	}
+
+	for i, char := range motJeu {
 		if i == Lrand1 || i == Lrand2 {
-			motatrouver = append(motatrouver, string(char))
+			motATrouver[i] = string(char)
 		} else {
-			motatrouver = append(motatrouver, string("_"))
+			motATrouver[i] = "_"
 		}
-
 	}
-	fmt.Println(motatrouver)
+	fmt.Println("Mot à deviner : ", strings.Join(motATrouver, " "))
 }
 
 func main() {
 	ChoixMot()
-	var Input string
-	fmt.Scanf(Input)
-	ToLower(Input)
-	for {
-		if IsAlpha(Input) == false {
-			fmt.Println("c'est pas une lettre.")
-			time.Sleep(2 * time.Second)
-			continue
-		} else {
-			for _, char := range MotJeu {
-				if Input == string(char) {
-					fmt.Println("Lettre trouvée")
-					time.Sleep(1 * time.Second)
-					motatrouver[char] = Input
-				} else {
-					fmt.Println("Pas la bonne lettre")
-					Tablettre = append(Tablettre, string(Input))
-					time.Sleep(1 * time.Second)
-				}
-			}
 
+	for essaisRestants > 0 {
+		var choix string
+		fmt.Println("Tapez 'lettre' pour proposer une lettre ou 'mot' pour proposer un mot entier : ")
+		fmt.Scanln(&choix)
+
+		if choix == "lettre" {
+			TryLetter()
+		} else if choix == "mot" {
+			TryMot()
+		} else {
+			fmt.Println("Choix invalide. Réessayez.")
+		}
+
+		if strings.Join(motATrouver, "") == motJeu {
+			fmt.Println("Félicitations ! Vous avez trouvé le mot :", motJeu)
+			break
 		}
 	}
 
+	if essaisRestants == 0 {
+		fmt.Println("Vous avez perdu. Le mot était :", motJeu)
+	}
 }
