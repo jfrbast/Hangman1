838a332bd2239a2c7364e4b8555e899f806e37f4
jfrbast <bastien.jouffre@ynov.com>
2024-09-28 13:33:29 +0200

    finitions et test

--- go.mod
+++ go.mod
@@ -2,8 +2,9 @@ module Hangman1
 
 go 1.23.0
 
+require github.com/gookit/color v1.5.4
+
 require (
-	github.com/gookit/color v1.5.4 // indirect
 	github.com/xo/terminfo v0.0.0-20210125001918-ca9a967f8778 // indirect
 	golang.org/x/sys v0.10.0 // indirect
 )
--- main.go
+++ main.go
@@ -10,6 +10,125 @@ import (
 	"time"
 )
 
+// Variables globales
+var diff int
+var tableauMot []string
+var motATrouver []string
+var motJeu string
+var lettresEssayees []string
+var essaisRestants = 10
+
+func TryLetter() {
+	var input string
+	fmt.Println("Proposez une lettre : ")
+	fmt.Scanln(&input)
+	input = ToLower(input)
+
+	if !IsAlpha(input) || len(input) != 1 {
+		color.Hex("#ff0000").Println("Ceci n'est pas une lettre valide.")
+		return
+	}
+
+	for _, l := range lettresEssayees {
+		if l == input {
+			fmt.Println("Vous avez déjà proposé cette lettre.")
+			return
+		}
+
+	}
+	lettresEssayees = append(lettresEssayees, input)
+
+	found := false
+	for i, char := range motJeu {
+		if input == string(char) {
+			motATrouver[i] = input
+			found = true
+		}
+	}
+
+	if found {
+		color.Hex("#2dc512").Println("Lettre trouvée !")
+
+	} else {
+		essaisRestants--
+		color.Hex("#ff0000").Println("Lettre incorrecte. Essais restants :", essaisRestants)
+
+	}
+
+}
+
+func TryMot() {
+	var input string
+	fmt.Println("Proposez le mot entier : ")
+	fmt.Scanln(&input)
+	input = ToLower(input)
+
+	if input == motJeu {
+		fmt.Println("Félicitations ! Vous avez deviné le mot :", motJeu)
+		os.Exit(0)
+	} else {
+		essaisRestants -= 2
+		color.Hex("#ff0000").Println("Mauvaise proposition. Essais restants :", essaisRestants)
+
+	}
+}
+
+func ToLower(s string) string {
+	result := ""
+	for _, char := range s {
+		if char >= 'A' && char <= 'Z' {
+			result += string(char + 32)
+		} else {
+			result += string(char)
+		}
+	}
+	return result
+}
+
+func IsAlpha(s string) bool {
+	for i := 0; i < len(s); i++ {
+		if !(s[i] >= 'a' && s[i] <= 'z') {
+			return false
+		}
+	}
+	return true
+}
+
+func difficulty() {
+	var input int
+
+	fmt.Println("Choisissez le niveau de difficulté.")
+	color.Hex("#2dc512").Println("1: Facile (10 essais + 3 indices)")
+	color.Hex("#ffe400").Println("2: Moyen (7 essais + 2 indices)")
+	color.Hex("#ff0000").Println("3: Difficile (5 essais + 1 indice)")
+	for {
+		_, err := fmt.Scanln(&input)
+		if err != nil {
+			color.Hex("#ff0000").Println("Erreur d'entrée, veuillez réessayer.")
+			continue
+		}
+		switch input {
+		case 1:
+			color.Hex("#2dc512").Println("Vous avez choisi le niveau Facile.")
+			essaisRestants = 10
+			diff = 1
+			return
+		case 2:
+			color.Hex("#ffe400").Println("Vous avez choisi le niveau Moyen.")
+			essaisRestants = 8
+			diff = 2
+			return
+		case 3:
+			color.Hex("#ff0000").Println("Vous avez choisi le niveau Difficile.")
+			essaisRestants = 6
+			diff = 3
+			return
+		default:
+			color.Hex("#ff0000").Println("Choix invalide, veuillez entrer 1, 2 ou 3.")
+		}
+	}
+}
+
 func affichage() {
 	time.Sleep(1 * time.Second)
 	switch essaisRestants {
@@ -21,7 +140,7 @@ func affichage() {
 		`)
 		time.Sleep(1 * time.Second)
 	case 9:
-		color.Hex("#e295ff").Println(`
+		color.Hex("#808080").Println(`
 
 			|
 			|
@@ -52,7 +171,7 @@ func affichage() {
 		`)
 		time.Sleep(1 * time.Second)
 	case 6:
-		color.Hex("#e295ff").Println(`
+		color.Hex("#808080").Println(`
 
 			_________
 			|       |
@@ -130,127 +249,13 @@ func affichage() {
 	}
 }
 
-func ToLower(s string) string {
-	result := ""
-	for _, char := range s {
-		if char >= 'A' && char <= 'Z' {
-			result += string(char + 32)
-		} else {
-			result += string(char)
-		}
-	}
-	return result
-}
-
-func IsAlpha(s string) bool {
-	for i := 0; i < len(s); i++ {
-		if !(s[i] >= 'a' && s[i] <= 'z') {
-			return false
-		}
-	}
-	return true
-}
-
-func difficulty() {
-	var input int
-
-	fmt.Println("Choisissez le niveau de difficulté.")
-	color.Hex("#2dc512").Println("1: Facile (10 essais + 3 indices)")
-	color.Hex("#ffe400").Println("2: Moyen (7 essais + 2 indices)")
-	color.Hex("#ff0000").Println("3: Difficile (5 essais + 1 indice)")
-	for {
-		_, err := fmt.Scanln(&input)
-		if err != nil {
-			color.Hex("#ff0000").Println("Erreur d'entrée, veuillez réessayer.")
-			continue
-		}
-		switch input {
-		case 1:
-			color.Hex("#2dc512").Println("Vous avez choisi le niveau Facile.")
-			essaisRestants = 10
-			diff = 1
-			return
-		case 2:
-			color.Hex("#ffe400").Println("Vous avez choisi le niveau Moyen.")
-			essaisRestants = 8
-			diff = 2
-			return
-		case 3:
-			color.Hex("#ff0000").Println("Vous avez choisi le niveau Difficile.")
-			essaisRestants = 6
-			diff = 3
-			return
-		default:
-			color.Hex("#ff0000").Println("Choix invalide, veuillez entrer 1, 2 ou 3.")
-		}
-	}
-}
-
-// Variables globales
-var diff int
-var tableauMot []string
-var motATrouver []string
-var motJeu string
-var lettresEssayees []string
-var essaisRestants = 10
-
-func TryLetter() {
-	var input string
-	fmt.Println("Proposez une lettre : ")
-	fmt.Scanln(&input)
-	input = ToLower(input)
-
-	if !IsAlpha(input) || len(input) != 1 {
-		color.Hex("#ff0000").Println("Ceci n'est pas une lettre valide.")
-		return
-	}
-
-	for _, l := range lettresEssayees {
-		if l == input {
-			fmt.Println("Vous avez déjà proposé cette lettre.")
-			return
-		}
-
-	}
-	lettresEssayees = append(lettresEssayees, input)
-
-	found := false
-	for i, char := range motJeu {
-		if input == string(char) {
-			motATrouver[i] = input
-			found = true
-		}
-	}
-
-	if found {
-		color.Hex("#2dc512").Println("Lettre trouvée !")
-		affichage()
-	} else {
-		essaisRestants--
-		color.Hex("#ff0000").Println("Lettre incorrecte. Essais restants :", essaisRestants)
-		affichage()
-	}
-	time.Sleep(1 * time.Second)
-	fmt.Println("Mot à deviner : ", strings.Join(motATrouver, " "))
-	time.Sleep(1 * time.Second)
-	fmt.Println("Lettres déjà proposées : ", strings.Join(lettresEssayees, ", "))
-	time.Sleep(1 * time.Second)
-}
-
-func TryMot() {
-	var input string
-	fmt.Println("Proposez le mot entier : ")
-	fmt.Scanln(&input)
-	input = ToLower(input)
-
-	if input == motJeu {
-		fmt.Println("Félicitations ! Vous avez deviné le mot :", motJeu)
-		os.Exit(0)
-	} else {
-		essaisRestants -= 2
-		color.Hex("#ff0000").Println("Mauvaise proposition. Essais restants :", essaisRestants)
-		affichage()
-	}
+func showMenu() {
+	color.Hex("#61e1f9").Println(`
+			======================================
+			1. Tapez 'L' pour deviner une lettre
+			2. Tapez 'M' pour deviner un mot complet
+			======================================
+	`)
 }
 
 func ChoixMot() {
@@ -299,9 +304,10 @@ func ChoixMot() {
 	}
 }
 
-func showMenu() {
+func main() {
+	difficulty()
+	ChoixMot()
 	color.Hex("#61e1f9").Println(`
-			======================================
 			 _    _                                          
 			| |  | |                                         
 		 	| |__| | __ _ _ __   __ _ _ __ ___   __ _ _ __    
@@ -309,20 +315,14 @@ func showMenu() {
 		 	| |  | | (_| | | | | (_| | | | | | | (_| | | | |  
 		 	|_|  |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|  
 					    __/  |
-					    |___/                  
-			======================================
-			1. Tapez 'L' pour deviner une lettre
-			2. Tapez 'M' pour deviner un mot complet
-			======================================
-	`)
-}
-
-func main() {
-	difficulty()
-	ChoixMot()
+					    |___/                 `)
 	time.Sleep(2 * time.Second)
 	for essaisRestants > 0 {
 		showMenu()
+		affichage()
+		time.Sleep(1 * time.Second)
+		fmt.Println("Lettres déjà proposées : ", strings.Join(lettresEssayees, ", "))
+		time.Sleep(1 * time.Second)
 		color.Hex("#1694d8").Println("Mot à deviner : ", strings.Join(motATrouver, " "))
 		time.Sleep(1 * time.Second)
 		var choix string
